fd530bbd249fcaad8a018f782b9fdf3a5f87d14c
Iadolphe007 <iadolphe007@gmail.com>
2024-01-31 16:50:08 +0200

    enzyme testing

--- 0x01-react_intro/task_3/dashboard/src/App.test.js
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App compent testing', () => {
+  it('App renders without crashing', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toBeDefined();
+  });
+
+  it('App renders a div with the class App-header', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("App-header")).toBeDefined();
+  });
+
+  it('App renders a div with the class App-body', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("App-body")).toBeDefined();
+  });
+
+  it('App renders a div with the class App-footer', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("App-footer")).toBeDefined();
+  });
+})
