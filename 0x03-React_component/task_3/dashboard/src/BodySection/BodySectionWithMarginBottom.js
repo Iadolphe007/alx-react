899e7630e0c92225fd197bc3489fe9fda9baeb97
Iadolphe007 <iadolphe007@gmail.com>
2024-02-08 13:33:09 +0000

    parent props

--- 0x03-React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ 0x03-React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
+import BodySection from "./BodySection";
+import "./BodySectionWithMarginBottom.css"
 
 class BodySectionWithMarginBottom extends Component {
     render(props) {
