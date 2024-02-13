f0fc75decc28b35badc30e4f87538997811c89fb
Iadolphe007 <iadolphe007@gmail.com>
2024-02-13 10:29:01 +0000

    codt styling

--- 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.js
@@ -2,15 +2,6 @@ import React from "react";
 import PropTypes from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
 
-
-const rowStyle = {
-    backgroundColor: "#f5f5f5ab",
-};
-
-const headerStyle = {
-    backgroundColor: "#deb5b545",
-};
-
 export default function CourseListRow({ isHeader, textFirstCell, textSecondCell}) {
     return(
         <tr style={css(styles.tr)}>
