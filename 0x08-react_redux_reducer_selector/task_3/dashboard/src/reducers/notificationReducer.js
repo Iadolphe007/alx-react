d437b3e19b589a5a3aafefc2eed53a1fc1e0526a
Iadolphe007 <iadolphe007@gmail.com>
2024-03-02 10:54:35 +0000

    adding notification reducer

--- 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/notificationReducer.js
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/notificationReducer.js
@@ -9,4 +9,40 @@ import {
     filter: 'DEFAULT',
   };
 
-  
+  const notifiticationsReducer = (state = initialNotificationState, action) => {
+    switch(action.type) {
+      case FETCH_NOTIFICATIONS_SUCCESS:
+        const updatedNotification = action.data.map(Notification => ({
+            ...Notification,
+            isRead: false,
+        }));
+        return {
+          ...state,
+          notifications: updatedNotification,
+        }
+      case MARK_AS_READ:
+        const updatedNotificationList = state.notifications.map(Notification => {
+          if (Notification.id === action.id){
+            return{
+              ...Notification,
+              isRead: true,
+            }
+          }
+          return Notification
+        });
+        return {
+          ...state,
+          notifications: updatedNotification
+        }
+      case SET_TYPE_FILTER:
+        return {
+          ...state,
+          filter: action.filter,
+        }
+      default:
+        break;
+    }
+    return state;
+  }
+
+  export default notifiticationsReducer
