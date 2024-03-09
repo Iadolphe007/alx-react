cc92d9ba377d25de3c68e925a3ac4318f91b1374
Iadolphe007 <iadolphe007@gmail.com>
2024-03-09 12:01:31 +0000

    dev tool

--- 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -1,26 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App/App";
+import { createStore, applyMiddleware, compose } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { Provider } from "react-redux";
-import uiReducer from "./reducers/uiReducer";
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import thunk from 'redux-thunk';
+import thunk from "redux-thunk";
+import App from "./App/App";
+import uiReducer, { initialState } from "./reducers/uiReducer";
+import { Map } from "immutable";
 
-const store = configureStore({
-  reducer: uiReducer, 
-  preloadedState: {
-    isNotificationDrawerVisible: false,
-    isUserLoggedIn: false,
-    user: {}
-  },
-  middleware: [...getDefaultMiddleware(), thunk],
-  devTools: process.env.NODE_ENV !== 'production',
-});
+// const store = createStore(
+//   uiReducer,
+//   Map(initialState),
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  uiReducer,
+  Map(initialState),
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-     <App />
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
