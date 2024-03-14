ecb512d47453a8bd4bf73245fdb52062d5e33c4f
Iadolphe007 <iadolphe007@gmail.com>
2024-03-14 12:49:19 +0000

    New Action Creator

--- 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.js
+++ 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.js
@@ -16,3 +16,31 @@ export default function setNotificationFilter (filter) {
     };
 }
 export const SetNotificationFilter = (index) => (dispatch(setNotificationFilter(index)));
+
+export const boundSetNotificationFilter = (filter) =>
+  dispatch(setNotificationFilter(filter));
+
+export const setLoadingState = (loading) => {
+  return {
+    type: SET_LOADING_STATE,
+    loading,
+  };
+};
+
+export const setNotifications = (data) => {
+  return {
+    type: FETCH_NOTIFICATIONS_SUCCESS,
+    data,
+  };
+};
+
+export const fetchNotifications = () => {
+  return (dispatch) => {
+    dispatch(setLoadingState(true));
+    return fetch("./notifications.json")
+      .then((res) => res.json())
+      .then((data) => dispatch(setNotifications(data)))
+      .catch((error) => {})
+      .finally(() => dispatch(setLoadingState(false)));
+  };
+}
